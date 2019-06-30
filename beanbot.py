7af69bded4061a6017fcf9eee480b79ca0543851
Kevin Zúñiga <kevin@amigocloud.com>
2019-06-30 12:08:35 -0500

    Several improvements
    Improve bot interface
    Add dynamic pre-filled values
    Add timezone support
    Change transaction format
    Fix some bugs

--- beanbot.py
+++ beanbot.py
@@ -5,94 +5,155 @@ import decimal
 import itertools
 import logging
 import os
-import re
 
-from telegram import (ReplyKeyboardMarkup, InlineKeyboardButton,
-                      InlineKeyboardMarkup)
-from telegram.ext import (Updater, CommandHandler, MessageHandler,
-                          Filters, RegexHandler, ConversationHandler,
-                          CallbackQueryHandler)
+import pytz
+from telegram import (InlineKeyboardButton, InlineKeyboardMarkup,
+                      ReplyKeyboardMarkup)
+from telegram.ext import (CallbackQueryHandler, CommandHandler,
+                          ConversationHandler, Filters, MessageHandler,
+                          Updater)
 
 
 # Token
 TOKEN = os.environ.get('BOT_TOKEN')
 
-
 # Logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 
 logger = logging.getLogger(__name__)
 
 
 # Models
-def create_new_transaction(**kwargs):
+PREFILLED_FIELDS = ('payee', 'account_1', 'account_2')
+
+
+def make_date(timezone):
+    timezone = pytz.timezone(timezone)
+    return datetime.datetime.now(tz=timezone).date()
+
+
+def make_new_transaction(user_config, **kwargs):
     transaction = {
-        'date': datetime.date.today(),
+        'date': make_date(user_config['timezone']),
         'info': '',
-        'payee': '',
-        'flag': '*',
-        'currency': 'PEN',
-        'account_1': 'Expenses:Stuff',
+        'payee': user_config['payee'][0],
+        'flag': user_config['flag'],
+        'currency': user_config['currency'],
+        'account_1': user_config['account_1'][0],
         'amount': decimal.Decimal(0),
-        'account_2': 'Assets:Cash',
+        'account_2': user_config['account_2'][0],
     }
     transaction.update(kwargs)
     return transaction
 
 
-def parse_message_transaction(text):
-    *info, amount, currency = text.split()
+def parse_message_transaction(text, user_config):
+    try:
+        *info, amount = text.strip().split()
 
-    return create_new_transaction(
-        info=' '.join(info),
-        amount=decimal.Decimal(amount),
-        currency=currency,
-    )
+        return make_new_transaction(
+            user_config,
+            info=' '.join(info),
+            amount=decimal.Decimal(amount),
+        )
+    except (ValueError, decimal.InvalidOperation) as ex:
+        raise ValueError from ex
 
 
-class Default:
-    payee = ['Metro', 'Sr. Roos', 'Grocery store', 'Restaurant', 'La Panetteria']
+def set_transaction_field(transaction, field, value):
+    transaction[field] = value.strip()
+
+
+def commit_transaction(transaction, user_data):
+    transaction_list = user_data.setdefault('transaction_list', [])
+    transaction_list.append(transaction)
+    del user_data['current_transaction']
 
 
 # Utils
-def make_main_inline_keyboard():
-    return InlineKeyboardMarkup([
-        [InlineKeyboardButton('Payee', callback_data='payee'),
-         InlineKeyboardButton('Info', callback_data='info')],
-        [InlineKeyboardButton('Asset', callback_data='account_1'),
-         InlineKeyboardButton('Expense', callback_data='account_2')],
-        [InlineKeyboardButton('Done!', callback_data='done')]
-    ])
+def get_field_name(field):
+    return {
+        'payee': 'Payee',
+        'account_1': "Expense account",
+        'account_2': "Asset account",
+        'info': "Info",
+    }[field]
+
+
+def set_default_user_config(user_data):
+    user_data['config'] = dict(
+        currency='USD',
+        timezone='UTC',
+        flag='*',
+        payee=['Store', 'Restaurant', 'Taxi'],
+        account_1=['Expenses:Stuff', 'Expenses:Food',
+                   'Expenses:Transportation'],
+        account_2=['Assets:Cash', 'Assets:Bank'],
+        favorites=None,
+    )
+
+
+def get_user_config(user_data):
+    if 'config' not in user_data:
+        set_default_user_config(user_data)
+    return user_data['config']
 
 
 class KeyboardFactory:
     @staticmethod
-    def _make_2x3_keyboard(labels, data):
-        pairs = zip(labels, data)
-        cols = 2
-        rows = len(data) // 2 + len(data) % 2
-
-        # return [
-        #     [InlineKeyboardButton(*pair)
-        #      for pair in itertools.islice(pairs, start, start + cols)]
-        #     for start in range(rows)
-        # ]
+    def _make_inline_keyboard(labels, data, cols=2):
+        button_kwargs = [dict(text=label, callback_data=datum)
+                         for label, datum in zip(labels, data)]
+        rows = len(data) // cols + len(data) % cols
 
         return InlineKeyboardMarkup([
-            [InlineKeyboardButton(labels[0], callback_data=data[0]),
-             InlineKeyboardButton(labels[1], callback_data=data[1])],
-            [InlineKeyboardButton(labels[2], callback_data=data[2]),
-             InlineKeyboardButton(labels[3], callback_data=data[3])],
-            [InlineKeyboardButton(labels[4], callback_data=data[4]),
-             InlineKeyboardButton(labels[5], callback_data=data[5])],
+            [InlineKeyboardButton(**kwargs)
+             for kwargs in itertools.islice(
+                 button_kwargs, start * cols, start * cols + cols)]
+            for start in range(rows)
         ])
 
     @staticmethod
-    def get_kb_for_payee():
-        data = ['payee_{}'.format(i) for i in range(len(Default.payee))]
-        return KeyboardFactory._make_2x3_keyboard(
-            (*Default.payee, '« Back'), (*data, 'go_back'))
+    def make_generic_inline_keyboard(type_, labels, cols=2, back=True,
+                                     back_data='go_back', back_label="« Back"):
+        n = len(labels)
+        data = [f'{type_}_{i}' for i in range(n)]
+
+        if back:
+            labels = labels.copy()
+            labels.append(back_label)
+            data.append(back_data)
+
+        return KeyboardFactory._make_inline_keyboard(labels, data, cols)
+
+    @staticmethod
+    def make_main_inline_keyboard():
+        buttons = [
+            ('Payee', 'payee'),
+            ('Info', 'info'),
+            ('Expense acct.', 'account_1'),
+            ('Asset acct.', 'account_2'),
+            ('Done', 'done'),
+        ]
+
+        return KeyboardFactory._make_inline_keyboard(
+            [button[0] for button in buttons],
+            [button[1] for button in buttons],
+        )
+
+
+def clear_inline_keyboard(update):
+    update.callback_query.edit_message_reply_markup(
+        InlineKeyboardMarkup([[]])
+    )
+
+
+def extract_index(data):
+    idx = data.split('_')[-1]
+    return int(idx)
 
 
 # States
@@ -100,11 +161,29 @@ WAITING_TRANSACTION, SELECTING_FIELD, FILLING_DATA = range(3)
 
 
 def format_transaction(transaction):
-    return ("{date} {flag} {payee} {info}\n"
-            "    {account_1}    {amount} {currency}\n"
-            "    {account_2}").format(
-                **transaction
+    header = ' - '.join(filter(None,
+                               (transaction['payee'], transaction['info'])))
+    return ('{date} {flag} {header}\n'
+            '    {account_1}    {amount:.2f} {currency}\n'
+            '    {account_2}').format(
+                **transaction,
+                header=header
             )
+
+
+def send_transaction_and_keyboard(update, transaction, inline_keyboard=None,
+                                  do_update=False):
+    if inline_keyboard is None:
+        inline_keyboard = KeyboardFactory.make_main_inline_keyboard()
+
+    formatted_transaction = format_transaction(transaction)
+
+    if do_update:
+        update.callback_query.edit_message_text(formatted_transaction,
+                                                reply_markup=inline_keyboard)
+    else:
+        update.message.reply_text(formatted_transaction,
+                                  reply_markup=inline_keyboard)
 
 
 def start(update, context):
@@ -116,21 +195,22 @@ def start(update, context):
 
 
 def register_transaction(update, context):
-    try:
-        transaction = parse_message_transaction(update.message.text)
-    except Exception as ex:
-        update.message.reply_text("I couldn't understand that "
-                                  "because {}".format(ex))
-        logger.exception(ex)
-        return WAITING_TRANSACTION
+    user_config = get_user_config(context.user_data)
+    message_text = update.message.text
 
-    inline_keyboard = make_main_inline_keyboard()
-    formatted_transaction = format_transaction(transaction)
+    try:
+        transaction = parse_message_transaction(message_text,
+                                                user_config)
+    except ValueError:
+        update.message.reply_text("Seems like the amount is missing.")
+    except Exception as ex:
+        update.message.reply_text("Sorry, I couldn't understand that.")
+        logger.exception("Could not parse transaction", ex)
+        return WAITING_TRANSACTION
 
     context.user_data['current_transaction'] = transaction
 
-    update.message.reply_text(formatted_transaction,
-                              reply_markup=inline_keyboard)
+    send_transaction_and_keyboard(update, transaction)
 
     return SELECTING_FIELD
 
@@ -138,67 +218,70 @@ def register_transaction(update, context):
 def selecting_field(update, context):
     button = update.callback_query.data
     current_transaction = context.user_data['current_transaction']
+    user_config = get_user_config(context.user_data)
 
-    if button == 'done':  # commit transaction
-        transaction_list = context.user_data.setdefault('transaction_list', [])
-
-        transaction_list.append(current_transaction)
-
-        del context.user_data['current_transaction']
-
-        update.callback_query.edit_message_reply_markup(
-            InlineKeyboardMarkup(
-                [[InlineKeyboardButton('Edit', callback_data='edit')]]
-            )
-        )
+    if button == 'done':
+        commit_transaction(current_transaction, context.user_data)
+        clear_inline_keyboard(update)
 
         update.callback_query.answer('Saved!')
 
         return WAITING_TRANSACTION
 
-    current_transaction[button] = '[{}]'.format(button.title())
-
     context.user_data['field'] = button
 
-    update.callback_query.edit_message_text(
-        format_transaction(current_transaction)
-    )
+    labels = []
+    msg = 'Write a value'
+    if button in PREFILLED_FIELDS:
+        labels = user_config[button]
+        msg = 'Select or write a value'
 
-    method_name = f'get_kb_for_{button}'
-    method = getattr(KeyboardFactory, method_name, None)
-    keyboard = method and method()
+    keyboard = KeyboardFactory.make_generic_inline_keyboard(button, labels)
 
-    if keyboard is not None:
-        update.callback_query.edit_message_reply_markup(
-            keyboard
-        )
+    send_transaction_and_keyboard(update, current_transaction,
+                                  inline_keyboard=keyboard, do_update=True)
 
-    update.callback_query.answer('Select one option')
+    update.callback_query.answer(msg)
 
     return FILLING_DATA
 
 
-def filling_data(update, context):
-    button = update.callback_query.data
+def filling_data_button(update, context):
+    button_data = update.callback_query.data
     current_transaction = context.user_data['current_transaction']
     current_field = context.user_data['field']
+    user_config = get_user_config(context.user_data)
 
-    try:
-        index = int(button.split('_')[-1])
-        list_ = getattr(Default, current_field, [])
-        current_transaction[current_field] = list_[index]
-    except Exception as ex:
-        update.callback_query.answer("That didn't work because {}".format(ex))
-        logger.exception(ex)
+    if button_data != 'go_back':
+        try:
+            index = extract_index(button_data)
+            value = user_config[current_field][index]
+        except Exception as ex:
+            update.callback_query.answer("Sorry, I couldn't process that.")
+            logger.exception(ex)
+        else:
+            set_transaction_field(current_transaction, current_field, value)
+            update.callback_query.answer("Got it!")
     else:
-        update.callback_query.edit_message_text(
-            format_transaction(current_transaction)
-        )
+        update.callback_query.answer()
 
-    update.callback_query.edit_message_reply_markup(
-        make_main_inline_keyboard()
-    )
+    send_transaction_and_keyboard(update, current_transaction,
+                                  do_update=True)
 
+    del context.user_data['field']
+    return SELECTING_FIELD
+
+
+def filling_data_text(update, context):
+    current_transaction = context.user_data['current_transaction']
+    current_field = context.user_data['field']
+    datum = update.message.text
+
+    set_transaction_field(current_transaction, current_field, datum)
+
+    send_transaction_and_keyboard(update, current_transaction)
+
+    del context.user_data['field']
     return SELECTING_FIELD
 
 
@@ -208,35 +291,34 @@ def error(update, context):
 
 
 def fallback(update, context):
-    update.message.reply_text('Fallback!')
+    update.message.reply_text("I got lost. Can try again?")
     return WAITING_TRANSACTION
 
 
 def main():
-    # Create the Updater and pass it your bot's token.
-    # Make sure to set use_context=True to use the new context based callbacks
-    # Post version 12 this will no longer be necessary
+    # Create the Updater and pass it your bot's token
     updater = Updater(TOKEN, use_context=True)
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
-    # Add conversation handler with the states CHOOSING, TYPING_CHOICE and TYPING_REPLY
+    # Add conversation handler with the states
     conv_handler = ConversationHandler(
         entry_points=[MessageHandler(Filters.text, register_transaction,
                                      pass_user_data=True)],
         states={
-            WAITING_TRANSACTION: [MessageHandler(Filters.text, register_transaction,
+            WAITING_TRANSACTION: [MessageHandler(Filters.text,
+                                                 register_transaction,
                                                  pass_user_data=True)],
-            SELECTING_FIELD: [CallbackQueryHandler(
-                                             selecting_field,
-                                             pass_user_data=True)],
-            FILLING_DATA: [CallbackQueryHandler(
-                                          filling_data,
-                                          pass_user_data=True),
-                           ],
+            SELECTING_FIELD: [CallbackQueryHandler(selecting_field,
+                                                   pass_user_data=True)],
+            FILLING_DATA: [CallbackQueryHandler(filling_data_button,
+                                                pass_user_data=True),
+                           MessageHandler(Filters.text, filling_data_text,
+                                          pass_user_data=True)],
         },
-        fallbacks=[MessageHandler(Filters.text, fallback, pass_user_data=True)],
+        fallbacks=[MessageHandler(Filters.text, fallback,
+                                  pass_user_data=True)],
     )
 
     dp.add_handler(conv_handler)
