22d952faf95679f60c06a9ae4031162243a02e63
Kevin Zúñiga <kevin.zun@gmail.com>
2019-12-18 14:07:19 -0500

    Remove username form User class

--- src/beanbot/users/models.py
+++ src/beanbot/users/models.py
@@ -1,9 +1,6 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models
 
 
 class User(AbstractUser):
-    username = models.TextField(unique=True)
-
     def __str__(self):
-        return f'[{self.id}] {self.username}'
+        return f'[{self.pk}] {self.username}'
